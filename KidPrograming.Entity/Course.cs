904c759ae85e6e7e8d6f410efa5ca4a93166787b
Tai Ha <huytainp@gmail.com>
2025-02-12 21:43:47 +0700

    createEntity

---
+++ KidPrograming.Entity/Course.cs
@@ -0,0 +1,19 @@
+﻿using KidPrograming.Core;
+
+namespace KidPrograming.Entity
+{
+    public class Course : BaseEntity
+    {
+        
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Subject { get; set; }
+        public string ThumbnailUrl { get; set; }
+        public decimal Price { get; set; }
+        public string Status { get; set; }
+
+        public virtual User Teacher { get; set; }
+        public virtual ICollection<Chapter> Chapters { get; set; }
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ KidPrograming.Entity/Notification.cs
@@ -0,0 +1,12 @@
+﻿using KidPrograming.Core;
+
+namespace KidPrograming.Entity
+{
+    public class Notification : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public bool IsRead { get; set; }
+        public virtual User User { get; set; }
+    }
+}
