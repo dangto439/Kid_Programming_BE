393c321d2d4a93f26fc13922de2c69be8ed5f659
Tai Ha <huytainp@gmail.com>
2025-03-02 15:01:07 +0700

    SetupRedis

---
+++ KidPrograming.Contract.Services/Interfaces/ICacheService.cs
@@ -0,0 +1,9 @@
+﻿namespace KidPrograming.Contract.Services.Interfaces
+{
+    public interface ICacheService
+    {
+        Task SetCacheResponseAsync(string key, object reponse, TimeSpan timeOut);
+        Task<string?> GetCacheResponseAsync(string key);
+        Task RemoveCacheResponseAsync(string pattern);
+    }
+}
