67bdb4e4da64301b35349090ad91d19287305ada
Minh Đức <139892190+duckonzz@users.noreply.github.com>
2025-03-09 20:38:25 +0700

    fix

--- KidPrograming.Contract.Services/Interfaces/ICourseService.cs
+++ KidPrograming.Contract.Services/Interfaces/ICourseService.cs
@@ -7,7 +7,7 @@ namespace KidPrograming.Contract.Services.Interfaces
 {
     public interface ICourseService
     {
-        Task<PaginatedList<ResponseCourseModel>> GetPage(bool? sortByTitle, bool? sortByPrice, CourseStatus? filterByStatus, string? searchByTitle, string? searchBySubject, string? teacherName, decimal? minPrice, decimal? maxPrice, int index, int pageSize);
+        Task<PaginatedList<ResponseCourseModel>> GetPage(bool? sortByTitle, bool? sortByPrice, CourseStatus? filterByStatus, string? searchById, string? searchByTitle, string? searchBySubject, string? teacherName, decimal? minPrice, decimal? maxPrice, int index, int pageSize);
         Task Create(CreateCourseModel model);
 
         Task Update(string id, UpdateCourseModel model);
