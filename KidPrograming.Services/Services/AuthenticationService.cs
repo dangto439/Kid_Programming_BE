a1ea9ee7318115a9898038e9c170fce946f7ec07
Minh Đức <139892190+duckonzz@users.noreply.github.com>
2025-03-10 15:01:19 +0700

    fix

--- KidPrograming.Services/Services/AuthenticationService.cs
+++ KidPrograming.Services/Services/AuthenticationService.cs
@@ -92,7 +92,7 @@ namespace KidPrograming.Services.Services
                 if (!string.IsNullOrEmpty(request.ParentId))
                 {
                     bool parentExists = await _unitOfWork.GetRepository<User>().Entities
-                        .AnyAsync(u => u.Id == request.ParentId && !u.DeletedTime.HasValue);
+                        .AnyAsync(u => u.Id == request.ParentId && u.Role == Enums.Role.Parent && !u.DeletedTime.HasValue);
                     if (!parentExists)
                     {
                         throw new ErrorException(StatusCodes.Status400BadRequest, ResponseCodeConstants.BADREQUEST, "Parent ID does not exist or was deleted.");
